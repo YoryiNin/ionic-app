ec39e7e260c53ea7664b16ba133fb3397973228e
Yoiryi <ratapika01@gmail.com>
2026-01-30 17:42:34 -0600

    Deploy web version

--- build/static/js/247.f79ea4a3.chunk.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 247.f79ea4a3.chunk.js.LICENSE.txt */
-"use strict";(globalThis.webpackChunkmyApp=globalThis.webpackChunkmyApp||[]).push([[247],{247(e,t,n){n.d(t,{startStatusTap:()=>a});var s=n(441),o=n(721),r=n(384);const a=()=>{const e=window;e.addEventListener("statusTap",()=>{(0,s.gv)(()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const i=(0,o.a)(a);i&&new Promise(e=>(0,r.c)(i,e)).then(()=>{(0,s.bN)(async()=>{i.style.setProperty("--overflow","hidden"),await(0,o.s)(i,300),i.style.removeProperty("--overflow")})})})})}}}]);
-//# sourceMappingURL=247.f79ea4a3.chunk.js.map
